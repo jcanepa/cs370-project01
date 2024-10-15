cdc85f448bf5f21513140aec21d25c36f17f0fdd
Julian Canepa <julian.l.canepa@gmail.com>
2024-10-15 15:05:07 -0600

    Working blowfish encryption.

--- enc.sh
+++ enc.sh
@@ -4,4 +4,4 @@ KEY="00112233445566778889aabbccddeeff"
 IV="0102030405060708"
 
 # Pass them into the openssl command using the built in generat
-openssl enc -bf-cbc -e -in plain.txt -out cipher.bin -pass pass:mysecurepassword -pbkdf2 -salt -provider legacy -provider default
+openssl enc -aes-128-cbc -e -in plain.txt -out cipher.bin -pass pass:foo -pbkdf2 -salt
